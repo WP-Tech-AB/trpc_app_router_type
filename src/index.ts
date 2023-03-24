b90ec5482828383596efca0b3d1fe64c901944da
Mo-Xiyad <xziyad92@gmail.com>
2023-03-24 10:50:55 +0100

    setup fake proxy client

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import { main } from './app';
-
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
+import fetch from 'cross-fetch';
+import { AppRouter, main } from './app';
 main();
+
+const run = async () => {
+  const trpcClient = createTRPCProxyClient<AppRouter>({
+    links: [httpBatchLink({ url: 'http:localhost:3000/api/trpc', fetch })]
+  });
+  const hello = await trpcClient.greeting.hello.query();
+  console.log({ hello });
+};
+run();
