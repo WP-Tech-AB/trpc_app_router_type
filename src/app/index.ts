bddbda39c3614eef9df6d8b5d5d84ff961b60cfa
Mo-Xiyad <xziyad92@gmail.com>
2023-03-24 11:13:48 +0100

    calling the api endpoint

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,24 @@
+import * as trpcExpress from '@trpc/server/adapters/express';
+import express from 'express';
 import { createGreetingRouter, GreetingRouter } from '../services/greeting';
 import { createTrpc, TrpcRouter } from '../trpc';
-
 export type AppRouter = TrpcRouter<{ greeting: GreetingRouter }>;
-
 export const main = async () => {
   const trpc = createTrpc();
   const appRouter: AppRouter = trpc.router({
     greeting: createGreetingRouter(trpc)
+  });
+  const app = express();
+  app.use(express.json());
+  app.get('/', (req, res) => {
+    res.sendStatus(200);
+  });
+  app.use('/trpc', trpcExpress.createExpressMiddleware({ router: appRouter }));
+  const port = 3000;
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, () => {
+      console.log(`Listening on http://localhost:${port}`);
+      resolve();
+    });
   });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import fetch from 'cross-fetch';
 import { AppRouter, main } from './app';
-main();
 
 const run = async () => {
+  await main();
+
   const trpcClient = createTRPCProxyClient<AppRouter>({
-    links: [httpBatchLink({ url: 'http:localhost:3000/api/trpc', fetch })]
+    links: [httpBatchLink({ url: 'http:localhost:3000/trpc', fetch })]
   });
   const hello = await trpcClient.greeting.hello.query();
   console.log({ hello });
 };
+
 run();
